16174129cc2e88a6d527b280d4133008fdc2c857
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-07-04 00:13:13 +0300

    Add 5 tast and update css class

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+buttonEl.addEventListener('click', changeColorBody);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function changeColorBody() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
 }
